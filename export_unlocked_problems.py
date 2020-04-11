c7ddea85fa9d6dfb8b4e4203f97f68df49043040
amard33p <amardeep.rox@gmail.com>
2020-04-11 18:15:13 +0530

    mark top 100

--- export_unlocked_problems.py
+++ export_unlocked_problems.py
@@ -2,55 +2,85 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
-from pathlib import Path
+import atexit
 
-BASEURL = 'https://leetcode.com'
-PROBLEMSET_URL = '/problemset/all/?difficulty='
+BASEURL = "https://leetcode.com"
+TOP_PROBLEMSET_URL = "/problemset/top-interview-questions/?difficulty="
+ALL_PROBLEMSET_URL = "/problemset/all/?difficulty="
 TBODY_SELECTOR = (
     By.XPATH,
     '//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[1]',
 )
 PAGINATION_SELECTOR = (
     By.XPATH,
-    '//*[@id="question-app"]/div/div[2]/div[2]/div[2]/table/tbody[2]/tr/td/span/select',
+    "//span/select[@class='form-control']",
 )
-DIFFICULTY = 'Easy'
+DIFFICULTY = "Easy"
 
 
-def fetch_table_source():
-    driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.get(f'{BASEURL}{PROBLEMSET_URL}{DIFFICULTY}')
-    driver.maximize_window()
+class LeetCodeParser:
+    def __init__(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get(f"{BASEURL}")
+        self.driver.maximize_window()
+        atexit.register(self._quit)
 
-    select = Select(driver.find_element(*PAGINATION_SELECTOR))
-    select.select_by_visible_text('all')
+    def _select_no_paginate(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(PAGINATION_SELECTOR)
+        )
+        select = Select(self.driver.find_element(*PAGINATION_SELECTOR))
+        select = Select(self.driver.find_element(*PAGINATION_SELECTOR))
+        select.select_by_visible_text("all")
 
-    _table = driver.find_element(*TBODY_SELECTOR)
-    table_source = _table.get_attribute('innerHTML')
+    def fetch_top_problems(self):
+        """
+        Get a list of https://leetcode.com/problemset/top-interview-questions
+        """
+        self.driver.get(f"{BASEURL}{TOP_PROBLEMSET_URL}{DIFFICULTY}")
+        self._select_no_paginate()
+        _table = self.driver.find_element(*TBODY_SELECTOR)
+        _rows = _table.find_elements(By.TAG_NAME, "tr")
+        _top_problems = []
+        for row in _rows:
+            _ = row.find_elements(By.TAG_NAME, "td")[1]
+            _top_problems.append(_.text)
+        return _top_problems
 
-    with open('table_source.html', 'w') as f:
-        f.write(table_source)
+    def fetch_all_unlocked_problems(self):
+        """
+        Fetch all unlocked problems
+        """
+        self.driver.get(f"{BASEURL}{ALL_PROBLEMSET_URL}{DIFFICULTY}")
+        self._select_no_paginate()
+        _table = self.driver.find_element(*TBODY_SELECTOR)
+        return _table.get_attribute("innerHTML")
 
-    driver.quit()
-
-
-def non_subscription_problems():
-    with open(f'leetcode_{DIFFICULTY.lower()}.md', 'w') as outfile:
-        outfile.write(f'## {DIFFICULTY}\n\n')
-        with open('table_source.html') as infile:
-            soup = BeautifulSoup(infile, 'html.parser')
-            rows = soup.find_all(['tr'])
+    def parse_and_write_output(self):
+        __top_problems = self.fetch_top_problems()
+        __all_problems_table_source = self.fetch_all_unlocked_problems()
+        with open(f"leetcode_{DIFFICULTY.lower()}.md", "w") as outfile:
+            outfile.write(f"## {DIFFICULTY}\n\n")
+            soup = BeautifulSoup(__all_problems_table_source, "html.parser")
+            rows = soup.find_all(["tr"])
             for row in rows:
-                cells = row.find_all('td')
-                if not (cells[2].find_all('i', {'class': 'fa-lock'})):
-                    print(
-                        f"[{cells[1].text}: {cells[2]['value']}]({BASEURL}{cells[2].find('a')['href']})"
-                    )
-                    outfile.write(
-                        f"- [{cells[1].text}: {cells[2]['value']}]({BASEURL}{cells[2].find('a')['href']})\n"
-                    )
-    Path('table_source.html').unlink()
+                cells = row.find_all("td")
+                if not (cells[2].find_all("i", {"class": "fa-lock"})):
+                    if cells[1].text in __top_problems:
+                        outfile.write(
+                            f"- [**{cells[1].text}: {cells[2]['value']}**]({BASEURL}{cells[2].find('a')['href']})\n"
+                        )
+                    else:
+                        outfile.write(
+                            f"- [{cells[1].text}: {cells[2]['value']}]({BASEURL}{cells[2].find('a')['href']})\n"
+                        )
 
-fetch_table_source()
-non_subscription_problems()
+    def _quit(self):
+        self.driver.quit()
+
+
+lc = LeetCodeParser()
+lc.parse_and_write_output()
